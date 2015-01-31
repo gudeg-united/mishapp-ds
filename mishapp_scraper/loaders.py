32ae1090b49bdd198f020f903114e0b14cfbfd87
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-02-01 01:18:17 +0700

    BmkgSpider: scrape earthquake and tsunami data

--- mishapp_scraper/loaders.py
+++ mishapp_scraper/loaders.py
@@ -3,19 +3,32 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from functools import partial
+
 import arrow
 from scrapy.contrib.loader import ItemLoader
 from scrapy.contrib.loader.processor import Join
 from scrapy.contrib.loader.processor import MapCompose
 
 
-def wib_to_utc(value):
+def wib_to_utc(value, format_):
     value = value.replace("WIB", "+07:00")
-    local = arrow.get(value, "DD-MM-YYYY HH:mm:ss Z")
+    local = arrow.get(value, format_)
     utc = local.to("UTC")
     return str(utc)
 
 
-class BmkgItemLoader(ItemLoader):
+class BmkgItemLoaderBase(ItemLoader):
     default_output_processor = Join()
-    date_time_in = MapCompose(wib_to_utc)
+
+
+class BmkgTsunamiItemLoader(BmkgItemLoaderBase):
+    date_time_in = MapCompose(
+        partial(wib_to_utc, format_="DD-MMM-YY HH:mm:ss Z"),
+        )
+
+
+class BmkgEarthquakeItemLoader(BmkgItemLoaderBase):
+    date_time_in = MapCompose(
+        partial(wib_to_utc, format_="DD/MM/YYYY HH:mm:ss Z"),
+        )
--- mishapp_scraper/spiders/bmkg.py
+++ mishapp_scraper/spiders/bmkg.py
@@ -4,26 +4,98 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
+import re
+
 import scrapy
+
 from mishapp_scraper.items import BmkgItem
-from mishapp_scraper.loaders import BmkgItemLoader
+from mishapp_scraper.loaders import BmkgEarthquakeItemLoader
+from mishapp_scraper.loaders import BmkgTsunamiItemLoader
+
+# BMKG domain name
+BMKG_DOMAIN = os.environ.get("BMKG_DOMAIN", "bmkg.go.id")
+
+# URL to BMKG page where earthquake data reside
+BMKG_EARTHQUAKE_URL = os.environ.get(
+    "BMKG_EARTHQUAKE_URL",
+    "http://bmkg.go.id/BMKG_Pusat/Gempabumi_-_Tsunami/Gempabumi/Gempabumi_Dirasakan.bmkg",  # noqa
+    )
+
+# URL to BMKG page where tsunami data reside
+BMKG_TSUNAMI_URL = os.environ.get(
+    "BMKG_TSUNAMI_URL",
+    "http://bmkg.go.id/BMKG_Pusat/Gempabumi_-_Tsunami/Tsunami.bmkg",
+    )
 
 
 class BmkgSpider(scrapy.Spider):
     name = "bmkg"
-    allowed_domains = ["bmkg.go.id"]
-    start_urls = (
-        "http://bmkg.go.id/BMKG_Pusat/Gempabumi_-_Tsunami/Gempabumi/Gempabumi_Terkini.bmkg",  # noqa
-        )
+    allowed_domains = [BMKG_DOMAIN]
+    start_urls = [BMKG_TSUNAMI_URL, BMKG_EARTHQUAKE_URL]
 
     def parse(self, response):
+        parser_map = {
+            BMKG_EARTHQUAKE_URL: self.parse_earthquake,
+            BMKG_TSUNAMI_URL: self.parse_tsunami,
+        }
+        callback = parser_map.get(response.url)
+        if callback:
+            return callback(response)
+
+    def parse_earthquake(self, response):
         for tr in iter(response.css("tbody tr")):
-            # first column is omitted
-            cols = tr.css("td::text").extract()[1:]
-            loader = BmkgItemLoader(BmkgItem(), selector=None)
-            loader.add_value("date_time", cols[0])
-            loader.add_value("lat", cols[1])
-            loader.add_value("lon", cols[2])
+            # Common data are extracted using `.//td/text()`,
+            # but date value is located under `td > a`,
+            # hence we're adding `|.//td/a/text()` in xpath expression
+            # to correctly extract date as well.
+            #
+            # For example::
+            #
+            #    <tr>
+            #        <td>118</td>
+            #        <td><a href="#">2000-01-02</a></td>
+            #    </tr>
+            cols = tr.xpath(".//td/text()|.//td/a/text()").extract()[1:6]
+            lat, lon = earthquake_latlon(cols[2])
+
+            loader = BmkgEarthquakeItemLoader(BmkgItem())
+            loader.add_value("date_time", " ".join(cols[:2]))
+            loader.add_value("lat", lat)
+            loader.add_value("lon", lon)
             loader.add_value("magnitudo", cols[3])
             loader.add_value("depth", cols[4])
+            loader.add_value("type", "earthquake")
             yield loader.load_item()
+
+    def parse_tsunami(self, response):
+        for tr in iter(response.css("tbody tr")):
+            cols = tr.css("td::text").extract()
+            lat, lon = cols[2].split(" - ")
+
+            loader = BmkgTsunamiItemLoader(BmkgItem())
+            loader.add_value("date_time", " ".join(cols[:2]))
+            loader.add_value("lat", lat)
+            loader.add_value("lon", lon)
+            loader.add_value("magnitudo", cols[3])
+            loader.add_value("depth", cols[4])
+            loader.add_value("type", "tsunami")
+            yield loader.load_item()
+
+
+# lat lon regex format for earthquake datasource
+eq_latlon_re = re.compile(r"(?P<lat>.+) L[B|S] (?P<lon>.+) B[B|T]")
+
+
+def earthquake_latlon(value):
+    """Gets latitude and longitude from a string.
+
+    :param value: Latlon string, for example `3.24 LS 128.96 BT`.
+    :returns: A tuple of lat and lon.
+    """
+    lat, lon = "", ""
+    rgx = eq_latlon_re.match(value)
+
+    if rgx:
+        lat, lon = rgx.groups()
+    return lat, lon
