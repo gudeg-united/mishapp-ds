e81c6c0ede753e0253ef5d8aa5b27a61a6cc482f
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-02-05 01:01:04 +0700

    add mongodb connection

--- mishapp_ds/scrape/pipelines.py
+++ mishapp_ds/scrape/pipelines.py
@@ -5,12 +5,29 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import scrapy.signals
+from scrapy.exceptions import NotConfigured
+from mongoengine import connect as mongo_connect
 
 
 class MongoPipeline(object):
+    def __init__(self, mongo_uri):
+        self.mongo_uri = mongo_uri
+        self._conn = None
+
+    @property
+    def conn(self):
+        if not self._conn:
+            self._conn = mongo_connect(host=self.mongo_uri)
+        return self._conn
+
     @classmethod
     def from_crawler(cls, crawler):
-        pipeline = cls()
+        mongo_uri = crawler.settings.get("MONGO_URI")
+
+        if not mongo_uri:
+            raise NotConfigured()
+
+        pipeline = cls(mongo_uri)
         crawler.signals.connect(pipeline.spider_opened,
                                 signal=scrapy.signals.spider_opened)
         crawler.signals.connect(pipeline.spider_closed,
